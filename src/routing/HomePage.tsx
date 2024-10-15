a2a6bd3e7ff90572237f468b0d05ca1d00f396c6
ma-shamshiri <ma.shamshiri@gmail.com>
2024-10-14 22:09:57 -0400

    Fix an issue with HomePage

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -4,12 +4,12 @@ import { BlockFooter } from "../components/BlockFooter"
 import { BlockHeroHome } from "../components/BlockHeroHome";
 import { BlockStatistics } from "../components/BlockStatistics";
 import { Navigationbar } from "../components/Navigationbar";
-import { BlockScrollingText } from "../components/BlockScrollingText";
+// import { BlockScrollingText } from "../components/BlockScrollingText";
 import { ParallaxSection } from "../components/ParallaxSection";
-import { PageTransition } from "../components/PageTransition";
-import { PageTransition2 } from "../components/PageTransition2";
-import NeoculturalCouture from "../components/NeoculturalCouture";
-import BlockHeroMosaic from "../components/BlockHeroMosaic";
+// import { PageTransition } from "../components/PageTransition";
+// import { PageTransition2 } from "../components/PageTransition2";
+// import NeoculturalCouture from "../components/NeoculturalCouture";
+// import BlockHeroMosaic from "../components/BlockHeroMosaic";
 import TicketCard from "../components/TicketCard";
 import BlockSponsors2024 from "../components/BlockSponsors2024";
 
