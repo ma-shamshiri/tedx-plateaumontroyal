994a99400a7ee353dd1d35ef2d356560e7eac845
ma-shamshiri <ma.shamshiri@gmail.com>
2024-11-01 06:37:04 -0400

    Modify a spleaer image

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.55lo0vdqan"
+    "revision": "0.aif4dqpd6dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
